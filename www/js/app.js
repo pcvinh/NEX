b94372a2f52e98183a1565d90dad40579ff49cd8
Phan Cao Vinh <caovinh04@yahoo.com>
2015-02-26 00:05:28 +0800

    Run ok. Closed phase 0: 1. Frame: tòan bộ fram bao gồm trang main có 2. Trang main: có nhiều thể lọai radar, 3. Trang upload: đã xử lý xong post content. 4. Trang me sẽ open xử lý sau: hiện tại mục tiêu chỉ là cơ bản: trang main để show subscribe & trang post để post thông tin lên server (server sẽ xử lý thuật tóan về mapping và chỉ quản lý các vấn đề sau này nếu client có cần thêm các chức năng advance)
    Các vấn đề còn tồn tại để phase 1 (launch thành bản demo production) làm:
    1. chưa có sử dụng cordova (gps, upload file, lấy local picture, lấy số dt để login)
    2. radar chưa làm chuyển giữa các lọai radar: home, favourit, here, map, hasktag.
    3. làm trang me

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('NEX', ['ionic', 'pubnub.angular.service', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, PubNub, RardarList) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,5 +15,110 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+	
+	PubNub.init({    
+		ssl           : true,
+		publish_key   : "pub-c-7b8f064f-cc65-4656-8d63-d6760bb6e0fe",
+		subscribe_key : "sub-c-abe025b6-b042-11e4-85c1-02ee2ddab7fe"
+	});
+	
+	RardarList.current_location_radar_init();
+	
   });
 })
+.service('RardarList', function($rootScope, $http, PubNub){
+	var self = this;
+	self.list = [];	
+		
+	function updatePostList(text) {
+		self.list.unshift(text);
+		$rootScope.$apply();
+		//$scope.PostList.pop();
+	}
+
+
+	this.current_location_radar_init = function() {	
+		var url = "http://127.0.0.1:5000?callback=JSON_CALLBACK";
+		var request = $http.jsonp(url);		
+		 
+		request.success(function(data) {
+			var my_channel = data.mychannel;
+			PubNub.ngSubscribe({ channel: my_channel });
+
+			$rootScope.$on(PubNub.ngMsgEv(my_channel), function(ngEvent, payload) {
+					updatePostList(payload.message);
+			});
+		});
+	}
+})
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/_tabs.html"
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.main', {
+      url: '/main',
+      views: {
+        'tab-main': {
+          templateUrl: 'templates/1.main.html',
+          controller: 'MainCtrl'
+        }
+      }
+    })
+
+    .state('tab.upload', {
+      url: '/upload',
+      views: {
+        'tab-upload': {
+          templateUrl: 'templates/2.upload.html',
+          controller: 'UploadCtrl'
+        }
+      }
+    })
+	
+    .state('tab.main.detail', {
+      url: '/main/:detailId',
+      views: {
+        'tab-detail': {
+          templateUrl: 'templates/4.detail.html',
+          controller: 'DetailCtrl'
+        }
+      }
+    })
+
+    .state('tab.dashboard', {
+      url: '/dashboard',
+      views: {
+        'tab-dashboard': {
+          templateUrl: 'templates/3.dashboard.html',
+          controller: 'DashboardCtrl'
+        }
+      }
+    })
+	
+	.state('tab.setting', {
+      url: '/setting',
+      views: {
+        'tab-setting': {
+          templateUrl: 'templates/5.setting.html',
+          controller: 'AccountCtrl'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/main');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,131 @@
+angular.module('starter.controllers', ['pubnub.angular.service'])
+
+.controller('MainCtrl', function($scope, RardarList) {
+	//RardarList.current_location_radar_init();
+	$scope.PostList =  RardarList.list;
+	
+	$scope.Here = function() {
+		
+	}
+	
+	$scope.Map = function() {
+		
+	}
+	
+	$scope.Home = function() {
+		
+	}
+	
+	$scope.Favourit = function() {
+		
+	}
+})
+
+.controller('UploadCtrl', function($scope, $http, $location) {
+	$scope.Title;
+	$scope.Content;
+	$scope.Post = function(message){
+	$http({
+		  method  : 'POST',
+		  url     : 'http://127.0.0.1:5000',
+			transformRequest: function(obj) {
+				var str = [];
+				for(var p in obj)
+				str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+				return str.join("&");
+			},
+		  data    : { Title : message.Title, Content: message.Content},  // pass in data as strings	
+		  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+		 }).success(function(data) {
+				$location.path('/main');
+				//alert("success");
+		  });
+	};
+	
+	$scope.Cancel = function(message) {
+		alert("Cancel " + message.Title);
+	}
+
+})
+
+.controller('DetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.detailId); // http request to get detail at server. 
+})
+
+.controller('DashboardCtrl', function($scope) {
+  
+  $scope.data = {
+    showDelete: false
+  };
+  
+  $scope.edit = function(item) {
+    alert('Edit Item: ' + item.id);
+  };
+  $scope.share = function(item) {
+    alert('Share Item: ' + item.id);
+  };
+  
+  $scope.moveItem = function(item, fromIndex, toIndex) {
+    $scope.items.splice(fromIndex, 1);
+    $scope.items.splice(toIndex, 0, item);
+  };
+  
+  $scope.onItemDelete = function(item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+  
+  $scope.items = [
+    { id: 0 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+    { id: 11 },
+    { id: 12 },
+    { id: 13 },
+    { id: 14 },
+    { id: 15 },
+    { id: 16 },
+    { id: 17 },
+    { id: 18 },
+    { id: 19 },
+    { id: 20 },
+    { id: 21 },
+    { id: 22 },
+    { id: 23 },
+    { id: 24 },
+    { id: 25 },
+    { id: 26 },
+    { id: 27 },
+    { id: 28 },
+    { id: 29 },
+    { id: 30 },
+    { id: 31 },
+    { id: 32 },
+    { id: 33 },
+    { id: 34 },
+    { id: 35 },
+    { id: 36 },
+    { id: 37 },
+    { id: 38 },
+    { id: 39 },
+    { id: 40 },
+    { id: 41 },
+    { id: 42 },
+    { id: 43 },
+    { id: 44 },
+    { id: 45 },
+    { id: 46 },
+    { id: 47 },
+    { id: 48 },
+    { id: 49 },
+    { id: 50 }
+  ];
+  
+});
